ac5e66994a48e3a84af051af36a10286fc36d51c
Taryn Sauer <taryn.sauer@gmail.com>
2014-02-24 16:11:42 -0600

    Add minimax methods

--- spec/tictactoe/ai_spec.clj
+++ spec/tictactoe/ai_spec.clj
@@ -2,4 +2,35 @@
   (:use speclj.core)
   (:use tictactoe.ai))
 
+(describe "ai"
 
+  (it "returns 1 for current player win"
+    (should= 1
+      (get-score ["X" "X" "X"
+                  "O" "O" 6
+                   7 8 9] "X")))
+
+  (it "returns -1 for current player loss"
+    (should= -1
+      (get-score ["X" "X" "X"
+                  "O" "O" 6
+                   7 8 9] "O")))
+
+  (it "returns 0 for non-winning board"
+    (should= 0 
+      (get-score ["X" "X" 3 
+                  "O" "O" 6
+                   7 8 9] "O")))
+
+  (it "returns 0 for non-winning board"
+    (should= 0 
+      (get-score ["X" "X" 3 
+                  "O" "O" 6
+                   7 8 9] "O")))
+
+  (it "returns 0 for non-winning board"
+    (should= {3 0, 6 0, 7 0, 8 0, 9 0} 
+      (open-cells-map ["X" "X" 3 "O" "O" 6 7 8 9])))
+
+
+)
--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -1,4 +1,49 @@
 (ns tictactoe.ai
-  (:use tictactoe.board))
+  (:use tictactoe.board)
+  (:use tictactoe.game))
 
+(def win 1)
 
+(def loss -1)
+
+(def tie 0)
+
+(def max-player-piece (atom "X"))
+
+(defn get-score [board piece]
+  (if (winning-game? board)
+    (if (= (opponent board) piece)
+      win 
+      loss)
+     tie)) 
+
+(defn apply-minimax [board, piece, depth]
+  (loop [board board, piece piece, depth depth]
+    (if (game-over? board)
+      (get-score board piece)
+      (let [best-score []]
+        (if (= piece @max-player-piece)
+          (doseq [cell-id (filter integer? board)] 
+            (let [score (recur (apply-move board (str cell-id)) (current-player board) (inc depth))]
+              (conj best-score (/ score depth))))
+            (apply max best-score)
+          (doseq [cell-id (filter integer? board)] 
+            (let [score (recur (apply-move board (str cell-id)) (current-player board) (inc depth))]
+              (conj best-score (/ score depth))))
+            (apply min best-score)))))) 
+
+(defn get-move-score [board, piece, cell]
+   (let [board (apply-move board (str cell)), depth 0] 
+     (apply-minimax board, piece, depth)))
+ 
+(defn rank-possible-moves [board piece]
+  (let [possible-moves (filter integer? board), scores {}]
+    (doseq [cell-id possible-moves] 
+      (assoc scores cell-id (get-move-score board, piece, cell-id)))))
+ 
+(defn get-best-move [board piece]
+  (key (apply max-key val (rank-possible-moves board piece)))) 
+
+(defn apply-ai-move [board piece]
+  (reset! max-player-piece piece)
+  (apply-move board (str (get-best-move board piece))))
