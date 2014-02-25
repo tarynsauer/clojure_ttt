f4a874460da6252af880dd5d2d70f8dc43062f19
Taryn Sauer <taryn.sauer@gmail.com>
2014-02-25 11:24:43 -0600

    Minimax working

--- spec/tictactoe/ai_spec.clj
+++ spec/tictactoe/ai_spec.clj
@@ -12,9 +12,9 @@
 
   (it "returns -1 for current player loss"
     (should= -1
-      (get-score ["X" "X" "X"
-                  "O" "O" 6
-                   7 8 9] "O")))
+      (get-score ["O" "O" "O"
+                  "X" "X" 6
+                   "X" 8 9] "X")))
 
   (it "returns 0 for non-winning board"
     (should= 0 
@@ -28,9 +28,89 @@
                   "O" "O" 6
                    7 8 9] "O")))
 
-  (it "returns 0 for non-winning board"
-    (should= {3 0, 6 0, 7 0, 8 0, 9 0} 
-      (open-cells-map ["X" "X" 3 "O" "O" 6 7 8 9])))
+ (it "takes winning space when one is available"
+   (should= 1 
+     (get-best-move [1 "X" "X"
+                     4 "O" "O"
+                     7 8 9 ] "X")))
+
+ (it "takes winning space when one is available"
+   (should= 7 
+     (get-best-move [1 "O" "X"
+                     4 "X" "O"
+                     7 8 9 ] "X")))
+
+ (it "blocks opponent win"
+   (should= 1 
+     (get-best-move [1 "X" "X"
+                     "X" "O" "O"
+                     "O" "X" 9 ] "O")))
+
+ (it "chooses win over block"
+   (should= 4 
+     (get-best-move [1 "X" "X"
+                     4 "O" "O"
+                     "O" "X" "X" ] "O")))
+
+ (it "chooses win over block or non-win"
+   (should= 4 
+     (get-best-move [1 "O" "O"
+                     4 "X" "X"
+                     "X" 8 "O" ] "X")))
+
+ (it "chooses block over multiple non-wins"
+   (should= 1 
+     (get-best-move [1 "O" "O"
+                     4 5 "X"
+                     "X" 8 9 ] "X")))
+
+ (it "chooses corner with multiple sides available"
+   (should= 1 
+     (get-best-move [1 2 3 
+                     4 "X" 6 
+                     7 8 9 ] "O")))
+
+ (it "chooses corner with multiple sides available"
+   (should= 1 
+     (get-best-move [1 "X" 3 
+                     4 5 6 
+                     7 8 9 ] "O")))
+
+ (it "chooses win when same cell is block and win"
+   (should= 1 
+     (get-best-move [1 "X" "X" 
+                     4 "O" 6 
+                     7 "X" "O" ] "O")))
+
+ (it "chooses center cell for the win"
+   (should= 5 
+     (get-best-move ["O" "X" "X" 
+                     4 5 6 
+                     7 "X" "O" ] "O")))
+
+ (it "chooses pre-emptive block/win move"
+   (should= 4 
+     (get-best-move ["X" 2 "O" 
+                     4 5 6 
+                     7 8 9 ] "X")))
+
+ (it "chooses pre-emptive block/win move"
+   (should= 9 
+     (get-best-move ["X" 2 "O" 
+                     "O" 5 6 
+                     "X" 8 9 ] "X")))
+
+ (it "chooses middle for win"
+   (should= 5 
+     (get-best-move ["X" 2 "O" 
+                     "O" 5 6 
+                     "X" "O" "X" ] "X")))
+
+ (it "chooses side for win"
+   (should= 8 
+     (get-best-move ["X" 2 "O" 
+                     "O" "O" 6 
+                     "X" 8 "X" ] "X")))
 
 
 )
--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -2,7 +2,7 @@
   (:use tictactoe.board)
   (:use tictactoe.game))
 
-(declare alphabeta)
+(declare minimax)
 
 (def win 1)
 
@@ -10,15 +10,11 @@
 
 (def tie 0)
 
-(def max-player-piece (atom "X"))
-
-(def neg-inf -999)
-
-(def pos-inf 999)
+(def current-player-piece (atom "X"))
 
 (defn get-score [board piece]
   (if (winning-game? board)
-    (if (= (opponent board) piece)
+    (if (= (opponent board) @current-player-piece)
       win 
       loss)
      tie)) 
@@ -26,27 +22,26 @@
 (defn apply-minimax [board, piece, depth]
   (if (game-over? board)
     (get-score board piece)
-    (if (= piece @max-player-value)
-      (apply min (minimax board, piece, depth)) 
-      (apply max (minimax board, piece, depth)))))
+    (let [best-scores (vec (minimax board, piece, depth))]
+      (if (= piece @current-player-piece)
+        (apply min best-scores)
+        (apply max best-scores)))))
 
-(defn minimax [board, piece, depth, best-score []]
-  (doseq [cell-id (filter integer? board)]
-    (let [score (/ (apply-minimax (apply-move board (str cell-id)), piece, (inc depth)) depth)]
-      (conj best-score score))))
+(defn minimax [board, piece, depth]
+  (for [cell-id (filter integer? board)] (/ (apply-minimax (apply-move board (str cell-id)), (current-player board) , (inc depth)) depth)))
 
 (defn get-move-score [board, piece, cell]
-   (let [board (apply-move board (str cell)), depth 0] 
+   (let [board (apply-move board (str cell)), depth 1] 
      (apply-minimax board, piece, depth)))
  
 (defn rank-possible-moves [board piece]
-  (let [possible-moves (filter integer? board), scores {}]
-    (doseq [cell-id possible-moves] 
-      (assoc scores cell-id (get-move-score board, piece, cell-id)))))
+  (let [possible-moves (filter integer? board)]
+    (for [cell possible-moves] 
+      (get-move-score board, piece, cell))))
  
 (defn get-best-move [board piece]
-  (key (apply max-key val (rank-possible-moves board piece)))) 
+  (reset! current-player-piece piece) 
+  (key (apply max-key val (reverse (into {} (map vector (vec (filter integer? board)) (vec (rank-possible-moves board piece))))))))
 
 (defn apply-ai-move [board piece]
-  (reset! max-player-piece piece)
   (apply-move board (str (get-best-move board piece))))
